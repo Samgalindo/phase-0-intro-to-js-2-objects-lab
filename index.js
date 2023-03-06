7d154d58c44df7a5fdfc11bcc7175e1e6c9d4133
Sam Galindo <ovosamgalindo@gmail.com>
2023-03-05 22:25:09 -0800

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution in this file!
+const employee = {
+    name : "Sam", 
+    streetAddress : "10830 Merritt Dr"
+}
+
+
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {
+        ...obj,
+        [key]: value,
+    }
+
+}
+
+const newEmployee = updateEmployeeWithKeyAndValue(
+    employee, "name", "Jafet"
+    )
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee.streetAddress = "12 Broadway"
+    
+    return employee
+}
+
+
+
+
+
+
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+
+    return newEmployee
+}
+
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name 
+    return employee
+}
